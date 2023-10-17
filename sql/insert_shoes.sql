8d01633462ef4d67a475a3f748e7649fc8787550
Asisipho <missfente@gmail.com>
2023-10-17 15:36:30 +0200

    image

--- sql/insert_shoes.sql
+++ sql/insert_shoes.sql
@@ -1,14 +1,16 @@
 INSERT INTO shoes (brand, shoe_name,color, shoe_size, price)
-VALUES ('Nike', 'Nike Gamma Force', 'Black', 5, 2249),
-       ('Puma', 'PUMA X RIPNDIP SLIPSTREAM SNEAKERS', 'White', 6, 1200),
-       ('Sketchers', 'Time Is Up','Red', 7, 2779),
-       ('New Balance', 'Balanciaga', 'Black', 4, 2999),
-       ('Adidas', 'Diddas me that', 'White', 9, 1500),
-       ('Puma','SOFTRIDER', 'Red',5, 1800),
-       ('Nike','Nike Airforce',  'Black', 6, 1599),
-       ('Adidas','Game Changer', 'White', 7, 3200),
-       ('Sketchers', 'Little Star', 'Red', 4, 800),
-       ('New Balance', 'Baby Shark', 'Black', 5, 999);
+VALUES ('Nike', 'Nike Gamma Force', 'Black', 5, 2249, 12),
+       ('Puma', 'PUMA X RIPNDIP SLIPSTREAM SNEAKERS', 'White', 6, 1200, 100),
+
+       ('Nike', 'Air Force 1 Low', 'yellow', 6, 1200, 100, 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4f5f1449-a491-4304-8693-0db22fce382c/free-metcon-5-womens-workout-shoes-h4Zl5h.png'),
+       ('Sketchers', 'Time Is Up','Red', 7, 2779, 2),
+       ('New Balance', 'Balanciaga', 'Black', 4, 2999, 56),
+       ('Adidas', 'Diddas me that', 'White', 9, 1500, 90),
+       ('Puma','SOFTRIDER', 'Red',5, 1800, 44),
+       ('Nike','Nike Airforce',  'Black', 6, 1599, 45),
+       ('Adidas','Game Changer', 'White', 7, 3200, 922),
+       ('Sketchers', 'Little Star', 'Red', 4, 800, 44),
+       ('New Balance', 'Baby Shark', 'Black', 5, 999, 1);
     
 
 /* SHOE BRANDS
