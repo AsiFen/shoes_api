3deb054e566fa5005944da857a5029997ed0849c
Asisipho <missfente@gmail.com>
2023-11-10 14:15:18 +0200

    added sign up function, endpoint is api/user

--- routes/cart_route.js
+++ routes/cart_route.js
@@ -1,18 +1,17 @@
 export default function CartRoute(shoesDB) {
 
     return {
-        getCart: async (req, res) => {
-            const shoeId = req.params.id;
-            let results = await shoesDB.getFromCart()
-            res.json(results)
-        },
-
         addCart: async (req, res) => {
             const shoeId = req.params.id;
             let results = await shoesDB.addToCart(shoeId)
             console.log(results, 'x');
             res.json(results)
+        },
+        getCart: async (req, res) => {
+            const shoeId = req.params.id;
+            let results = await shoesDB.getFromCart()
+            console.log(results);
+            res.json(results)
         }
-
     }
 }
